49c82e1e37e3aa9383dac48a5533a61acf3d9a9f
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-01-31 00:36:28 +0530

    Completed the front page

--- findjob/app/Http/Controllers/HomeController.php
+++ findjob/app/Http/Controllers/HomeController.php
@@ -1,13 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class HomeController extends Controller
-{
-    //
-    public function index(){
-        
+{   //  this method will show our home page
+    public function index()
+    {
+    return view('front.home');
     }
 }
--- findjob/routes/web.php
+++ findjob/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+// Route::get('/contact',[HomeController::class,'contact'])->name('contact');
