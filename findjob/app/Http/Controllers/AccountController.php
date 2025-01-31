8a1cd6387424d6f590425580a74dabb74f523db9
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-02-01 00:14:54 +0530

    Registation created

--- findjob/app/Http/Controllers/AccountController.php
+++ findjob/app/Http/Controllers/AccountController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    //This method will show user registation page
+    public function registration(){
+        return view('front.account.registration');
+
+    }
+    //this method will show user login page
+    public function login(){
+
+    }
+
 }
--- findjob/routes/web.php
+++ findjob/routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AccountController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get('/',[HomeController::class,'index'])->name('home');
-
+Route::get('/account/register',[AccountController::class,'registration'])->name('account.registration');
 // Route::get('/contact',[HomeController::class,'contact'])->name('contact');
